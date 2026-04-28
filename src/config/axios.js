e44b6392234f747be61f5d55463025a7c7204ee7
kavya <kavyabanni29@gmail.com>
2026-04-28 12:54:55 +0530

    Updatd files

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
     // baseURL: 'http://localhost:3050'
-    baseURL: 'https://expense-js7r.onrender.com'
+    baseURL: 'https://expenseback-6.onrender.com',
 })
